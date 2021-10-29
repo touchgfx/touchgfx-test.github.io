6af2e1d07950cd602af537b90fe0c40d593d4175
Victor Gram POULSEN <victor.poulsen@st.com>
2021-10-29 07:56:55 +0200

    Updated touchgfx documentation

--- js/redirect.js
+++ js/redirect.js
@@ -1,6 +1,12 @@
 if (window && window.location) {
+  let version_regex = /^touchgfx-test\.github\.io\/([0-9]*.[0-9]*)/
   // && window.location.pathname.endsWith('/') && window.location.pathname !== '/'
   let path = window.location.pathname
-  console.log(path)
+  let match = path.match(version_regex)
+  console.log(match)
+  if (match == null) {
+    let new_path = path.replace("touchgfx-test.github.io", "touchgfx-test.github.io/4.19")
+    console.log(new_path)
+  }
   // window.history.replaceState('', '', window.location.pathname.substr(0, window.location.pathname.length - 1) + window.location.hash)
 }
